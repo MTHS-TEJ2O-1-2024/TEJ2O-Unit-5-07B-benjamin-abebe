99982d5d7c4dc34a2e258835d619067e6a4b18a6
benjamin-abebe <benjamin.abebe@mths.ca>
2024-10-23 11:21:49 -0400

    Edited Typescript version.

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Benjamin Abebe
+ * Created on: Oct 2024
+ * This program allows the sorvo to move back and forth
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    robotbit.Servo(servoNumber1, 34)
+    basic.clearScreen()
+    basic.showString('30')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servoNumber1, 160)
+    basic.clearScreen()
+    basic.showString('160')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
